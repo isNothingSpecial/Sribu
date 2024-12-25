5a8e2fab9e9354c75eb1e5c75e403c3de6ea983c
Bagus Rahma AC <46398272+isNothingSpecial@users.noreply.github.com>
2024-12-25 21:49:52 +0700

    Update Profile Kelompok.py

--- pages/Profile Kelompok.py
+++ pages/Profile Kelompok.py
@@ -2,41 +2,37 @@ import streamlit as st
 from streamlit_webrtc import webrtc_streamer
 
 # Menggunakan HTML dan CSS untuk membuat header dan subheader rata tengah
+st.header('KELOMPOK 1')
+
 st.markdown(
     """
-    <h1 style='text-align: center;'>KELOMPOK 1</h1>
+    <h3 style='text-align: center;'>Bagus Rahma Aulia Chandra</h3>
+    <h4 style='text-align: center;'>Ketua Kelompok 1</h4>
     """, 
     unsafe_allow_html=True
 )
 
 # Membuat tiga kolom
-col1, col2 = st.columns(2)
+col1, col2, col3 = st.columns(3)
 
-# Menampilkan konten di setiap kolom
 with col1:
-    st.header("Bagus Rahma Aulia Chandra")
-    st.write("Ketua Kelompok")
+    st.header("Tannu Wibowo")
+    st.write("Anggota Kelompok 1")
 
 with col2:
-    st.header("Tannu Wibowo")
-    st.write("Anggota")
-
-col3, col4 = st.columns(2)
+    st.header("Annisa Firdaus Nasution")
+    st.write("Anggpta Kelompok 1")
 
 with col3:
-    st.header("Annisa Firdaus Nasution")
-    st.write("Anggpta")
+    st.header("Ismi Nurhadiyanti Rusmana")
+    st.write("Anggpta Kelompok 1")
+
+st.subheader("Supported By :")
+
+col4, col5 = st.columns(2)
 
 with col4:
-    st.header("Ismi Nurhadiyanti Rusmana")
-    st.write("Anggpta")
-
-st.write("Supported By :")
-
-col5, col6 = st.columns(2)
-
-with col5:
      st.image("Sribu.png", use_column_width=True)
 
-with col6:
+with col5:
      st.image("ds.png", use_column_width=True)
