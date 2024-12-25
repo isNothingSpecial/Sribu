599a464a73bd14fbf502e6e376ac38c86ccff97b
Bagus Rahma AC <46398272+isNothingSpecial@users.noreply.github.com>
2024-12-25 14:28:27 +0700

    Update Overview.py

--- Overview.py
+++ Overview.py
@@ -20,20 +20,30 @@ st.markdown(
     """, 
     unsafe_allow_html=True
 )
-st.image("ds.png", use_column_width=True)
 
-st.markdown(''' Sribu adalah platform pasar daring yang menghubungkan pemilik bisnis dengan pekerja lepas (freelancer) di berbagai bidang, termasuk desain grafis, pemrograman web, video, foto, audio, penulisan, terjemahan, pemasaran, dan iklan. Didirikan pada September 2011 oleh Ryan Gondokusumo dan Wenes Kusnadi, perusahaan ini berkantor pusat di Jakarta, Indonesia.
+# Membuat tiga kolom
+col1, col2, col3,col4 = st.columns(3)
 
-Sejarah Singkat:
+# Menampilkan konten di setiap kolom
+with col1:
+st.image("Sribu.png", use_column_width=True)
 
+with col2:
+st.write(''' Sribu atau dulu bernama Sribulancer adalah platform pasar daring yang menghubungkan pemilik bisnis dengan pekerja lepas (freelancer) di berbagai bidang, termasuk desain grafis, pemrograman web, video, foto, audio, penulisan, terjemahan, pemasaran, dan iklan. Didirikan pada September 2011 oleh Ryan Gondokusumo dan Wenes Kusnadi, perusahaan ini berkantor pusat di Jakarta, Indonesia.''')
+
+with col3:
+st.markdown('''Sejarah Singkat Sribu :
 - 2011: Sribu diluncurkan sebagai platform kontes desain yang berfokus pada pasar Indonesia, menawarkan berbagai kategori seperti desain logo, desain kemasan, dan desain interior.
 - 2012: Menerima pendanaan awal dari East Ventures, yang memungkinkan ekspansi layanan.
 - 2014: Mendapat investasi tambahan dari Asteria Japan dan memperluas kategori layanan untuk mencakup lebih dari sekadar desain grafis.
 - 2018: Menerima pendanaan dari Crowdworks, pasar freelance terbesar di Jepang.
 - 2022: Diakuisisi oleh Mynavi Japan dan menjadi anak perusahaan mereka.
+''')
 
+with col4
+st.markdown('''Pencapaian Sribu :
 Hingga tahun 2022, Sribu telah melayani lebih dari 30.000 klien dengan komunitas freelancer yang dikurasi secara ketat untuk memastikan kualitas dalam komunikasi, ketepatan waktu, dan hasil kerja.
-Sribu juga telah menerima beberapa penghargaan, termasuk Indonesia ICT Awards 2013 dan SparxUp Award 2011. 
+Sribu juga telah menerima beberapa penghargaan, termasuk Indonesia ICT Awards 2013 dan SparxUp Award 2011
 ''')
 
 st.markdown('''Data diatas masih berupa RAW data,yang mana nantinya akan diolah melalui beberapa proses antara lain:  
