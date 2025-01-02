3788649c98a9eaf1b967fea4df02fbd7eae0d373
Bagus Rahma AC <46398272+isNothingSpecial@users.noreply.github.com>
2025-01-02 22:00:06 +0700

    Update Uji Coba Dropdown Page.py

--- pages/Uji Coba Dropdown Page.py
+++ pages/Uji Coba Dropdown Page.py
@@ -15,3 +15,18 @@ st.markdown(
     """, 
     unsafe_allow_html=True
 )
+
+def run():
+    st.title("Uji Coba Dropdown Page")
+
+    # Dropdown untuk sub-pages
+    sub_page_choice = st.selectbox("Choose Sub-page", ["Literatur", "Profil Kelompok"])
+
+    # Memuat modul berdasarkan sub-page yang dipilih
+    if sub_page_choice == "Literatur":
+        sub_module = importlib.import_module("pages.page_1a")
+    elif sub_page_choice == "Profil Kelompok":
+        sub_module = importlib.import_module("pages.page_1b")
+    
+    # Jalankan fungsi `run` dari modul sub-page
+    sub_module.run()
