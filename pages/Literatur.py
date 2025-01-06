9fb9b66d4542145740425e6b5c2ef2979e22ad43
Bagus Rahma AC <46398272+isNothingSpecial@users.noreply.github.com>
2025-01-06 19:11:52 +0700

    Update Literatur.py

--- pages/Literatur.py
+++ pages/Literatur.py
@@ -251,7 +251,8 @@ elif literatur == 'Crosscheck and Deleting Anomali':
     2. Crosscheck ulang dengan pemilik data
     3. Melakukan Penghapusan Data-Data yang tidak diperlukan,rancu,ataupun anomali
     """)
-    st.write('''Dimana dalam dataset ini memiliki beberapa data yang termasuk anomali,dan data yang tidak diperlukan dimana diantaranya adalah :
+    st.markdown("""
+    Dimana dalam dataset ini memiliki beberapa data yang termasuk anomali,dan data yang tidak diperlukan dimana diantaranya adalah :
     - Penghapusan Data yang memiliki total_paid 0
         -Mengapa dihapus,karena dalam proses RFM,dimana akan menghitung akumulasi dari Frequentcy yaitu Seberapa sering pelanggan melakukan transaksi dan Monetary,atau total uang yang sudah di keluarkan selama bertransaksi menggunakan platform Sribu,sehingga,agar murni hanya pelanggan yang memiliki total_paid yang jelas.
     - Penghapusan Data dimana pada kolom client_user_id dimana diawalnya tanpa nama,dan diimputasi dengan kata-kata 'No_one'.
@@ -259,7 +260,7 @@ elif literatur == 'Crosscheck and Deleting Anomali':
     - Penghapusan Data pada kolom Type dengan nama 'deposit'
         - Mengapa dihapus,dimana setelah melakukan crosscheck ulang dengan pemilik data,yakni Sribu mendapatkan jawaban dimana data pada kolom Type dengan nama 'deposit',memiliki definisi Client mentransfer sejumlah uang dan uang tersebut menjadi deposit untuk membayar jasa yg ada di platform. Karena mentransfer sejumlah uang makanya ada dokumen invoices untuk pencatatan finance
         Sehingga kolom Type dengan nama 'deposit',bukan merupakan sebuah data transaksi tapi hanya sekedar transfer uang dan dicatat ke dalam pembukuan keuangan,sehingga 'deposit' memiliki pencatatan invoice.
-    ''')
+    """)
     
 
 elif literatur == 'Algoritma':
